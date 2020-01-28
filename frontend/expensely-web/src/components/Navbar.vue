3b6cc8e3f6049124c40c6cde1f17ea76ba9c6b35
sdawn29 <snehasishdawn29@gmail.com>
2020-01-28 19:00:57 +0530

    Created frontend Part and added Add expense module

---
+++ frontend/expensely-web/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="logo">Expense<span style="color: #667EEA; font-weight: bold">ly.</span></div>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.logo {
+    font-size: 2em;    
+}
+</style>
---
+++ frontend/expensely-web/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <Navbar/>
+    <b-row>
+      <b-col cols="8">Expenses</b-col>
+      <b-col cols="12" md="4">
+        <AddExpense/>
+
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '../components/Navbar.vue'
+import AddExpense from '../components/AddExpense.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Navbar,
+    AddExpense
+  }
+}
+</script>
