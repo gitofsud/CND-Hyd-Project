4f614ca3f3d3c710a6f6b31549a18dfd89e9637c
sdawn29 <snehasishdawn29@gmail.com>
2020-01-30 19:12:37 +0530

    Added Major functionalities.

--- frontend/expensely-web/src/components/AddExpense.vue
+++ frontend/expensely-web/src/components/AddExpense.vue
@@ -1,8 +1,11 @@
 <template>
   <div> 
-      <b-card title="Add Expense" sub-title="Add a new expense to your list" bg-variant="light">
+      <b-card sub-title="Add a new expense to your list" header="featured" header-bg-variant="primary" header-text-variant="white" border-variant="primary" class="rounded">
+          <template v-slot:header>
+      <h4 class="mb-0">Add Expense</h4>
+    </template>
         <b-card-text>
-            <b-form>
+            <b-form @submit.prevent="onSubmit">
                 <h6>    
                     <b-form-group
                         id="input-group-1"
@@ -15,6 +18,7 @@
                         type="text"
                         required
                         placeholder="Enter title"
+                        v-model="item.title"
                         ></b-form-input>
                     </b-form-group>
                 </h6>
@@ -23,13 +27,28 @@
                     <b-form-group id="input-group-2" label="Amount" label-for="input-2" description="The amount of money spend.">
                         <b-form-input
                         id="input-2"
+                        type="number"
                         required
                         placeholder="Enter amount"
+                        v-model="item.amt"
                         ></b-form-input>
                     </b-form-group>
                 </h6>
+                
+                <h6>
+                    <b-form-group id="input-group-3" label="Category" label-for="input-3" description="A category to categorise your expense">
+                        <b-form-select v-model="item.category" :options="options"></b-form-select>
+                    </b-form-group>
+                </h6>
 
-                <b-button type="submit" variant="primary">Add Expense</b-button>
+                <b-button type="submit" variant="primary" v-if="!loading">
+                    Add Expense
+                </b-button>
+
+                <b-button disabled variant="primary" v-if="loading">
+                    <b-spinner small label="Spinning"></b-spinner>
+                     Adding Expense
+                </b-button>
             </b-form>
         </b-card-text>
     </b-card>
@@ -37,8 +56,49 @@
 </template>
 
 <script>
-export default {
+import axios from "axios";
 
+export default {
+    data() {
+        return {
+            loading:false,
+            item:{
+                amt:'',
+                category:null,
+                title:'',
+            },
+            selected: null,
+            options: [
+            { value: null, text: 'Please select a category' },
+            { value: 'Bill', text: 'Bill' },
+            { value: 'Entertainment', text: 'Entertainment' },
+            { value: 'Food', text: 'Food' },
+            { value: 'Travel', text: 'Travel' },
+            { value: 'Personal', text: 'Personal' }
+            ]
+        }
+    },
+
+    methods: {
+        async onSubmit() {
+            this.loading=true
+            console.log("called")
+            const config = {
+                method: "post",
+                url: "https://0xq3l9bey8.execute-api.us-east-2.amazonaws.com/dev/expense",
+                data: this.item
+            };
+            try {
+                await axios(config);
+                this.loading = false;
+            } catch (error) {
+                console.log(error)
+            }
+            this.item.title = "";
+            this.item.amt = "";
+            this.item.category = null;
+        }
+    }
 }
 </script>
 
---
+++ frontend/expensely-web/src/components/ExpenseCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+      <b-card class="mb-4 rounded hoverable" >
+          <b-row>
+              <b-col>
+                <div id="amount">
+                    <h1 class="text-primary">
+                        ₹{{amount}}
+                    </h1>
+                    
+                </div>
+              </b-col>
+              <b-col>
+                  <center>
+                    <img src="../assets/car.svg" alt="" width="100%" v-if="category == 'Travel'">
+                    <img src="../assets/clapper.svg" alt="" width="90%" v-if="category == 'Entertainment'">
+                    <img src="../assets/food.svg" alt="" width="100%" v-if="category == 'Food'">
+                    <img src="../assets/bill.svg" alt="" width="100%" v-if="category == 'Bills'">
+                    <img src="../assets/parachute.svg" alt="" width="100%" v-if="category == 'Personal'">
+                  </center>
+              </b-col>
+          </b-row>
+            <h5>{{title}}</h5>
+            <b-badge variant="warning" class="p-2" v-if="category == 'Travel'">{{category}}</b-badge>
+            <b-badge variant="info" class="p-2" v-if="category == 'Entertainment'">{{category}}</b-badge>
+            <b-badge variant="primary" class="p-2" v-if="category == 'Food'">{{category}}</b-badge>
+            <b-badge variant="danger" class="p-2" v-if="category == 'Bills'">{{category}}</b-badge>
+            <b-badge variant="success" class="p-2" v-if="category == 'Personal'">{{category}}</b-badge>
+      </b-card>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: [
+    "id",
+    "title",
+    "amount",
+    "category",
+  ],
+  
+}
+</script>
+
+<style>
+
+.hoverable:hover {
+  cursor: pointer;
+  background-color: #EDF2F7;
+}
+</style>
